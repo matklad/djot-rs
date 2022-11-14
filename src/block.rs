c3fbfb5f223c8ffd2dfa7ec4542afb5bcf976d4f
Aleksey Kladov <aleksey.kladov@gmail.com>
2022-11-14 01:02:16 +0000

    .

--- src/block.rs
+++ src/block.rs
@@ -7,12 +7,6 @@ use crate::{
   Match, ParseOpts, Warn,
 };
 
-struct Container {
-  spec: &'static Spec,
-  inline_parser: inline::Parser,
-  indent: usize,
-}
-
 #[derive(Default)]
 pub struct Parser {
   warn: Option<Warn>,
@@ -22,7 +16,6 @@ pub struct Parser {
   starteol: usize,
   endeol: usize,
   pub(crate) matches: Vec<Match>,
-  containers: Vec<Container>,
   pos: usize,
   last_matched_container: usize,
   opts: ParseOpts,
@@ -31,83 +24,85 @@ pub struct Parser {
   pub(crate) debug: String,
 }
 
-struct Spec {
-  #[allow(unused)]
-  name: &'static str,
-  is_para: bool,
-  content: &'static str,
-  cont: fn(&mut Parser) -> bool,
-  open: fn(&mut Parser) -> bool,
-  close: fn(&mut Parser),
+trait Container {
+  fn content(&self) -> &'static str;
+  fn inline_parser(&mut self) -> Option<&mut inline::Parser> {
+    None
+  }
+  fn open(p: &mut Parser) -> Option<Box<dyn Container>>
+  where
+    Self: Sized;
+  fn cont(&mut self, p: &mut Parser) -> bool;
+  fn close(self: Box<Self>, p: &mut Parser);
 }
 
-impl Spec {
-  fn cont(&self, p: &mut Parser) -> bool {
-    (self.cont)(p)
+const CONTAINERS: &[fn(&mut Parser) -> Option<Box<dyn Container>>] = &[Para::open, CodeBlock::open];
+
+struct Para {
+  inline_parser: inline::Parser,
+}
+
+impl Container for Para {
+  fn content(&self) -> &'static str {
+    "inline"
   }
-  fn open(&self, p: &mut Parser) -> bool {
-    (self.open)(p)
+  fn inline_parser(&mut self) -> Option<&mut inline::Parser> {
+    Some(&mut self.inline_parser)
   }
-  fn close(&self, p: &mut Parser) {
-    (self.close)(p)
+  fn open(p: &mut Parser) -> Option<Box<dyn Container>>
+  where
+    Self: Sized,
+  {
+    p.add_match(p.pos, p.pos, Comp::Para.add());
+    Some(Box::new(Para {
+      inline_parser: inline::Parser::new(p.subject.clone(), p.opts.clone(), p.warn.clone()),
+    }))
+  }
+
+  fn cont(&mut self, p: &mut Parser) -> bool {
+    p.find("^%S").is_match
+  }
+
+  fn close(mut self: Box<Self>, p: &mut Parser) {
+    p.matches.extend(self.inline_parser.get_matches());
+    p.add_match(p.pos - 1, p.pos - 1, Comp::Para.sub())
   }
 }
 
-static SPECS: &[Spec] = &[
-  Spec {
-    name: "para",
-    is_para: true,
-    content: "inline",
-    cont: |p| p.find("^%S").is_match,
-    open: |p| {
-      p.add_container(Container {
-        spec: &SPECS[0],
-        inline_parser: inline::Parser::new(p.subject.clone(), p.opts.clone(), p.warn.clone()),
-        indent: 0,
-      });
-      p.add_match(p.pos, p.pos, Comp::Para.add());
-      true
-    },
-    close: |p| {
-      p.get_inline_matches();
-      p.add_match(p.pos - 1, p.pos - 1, Comp::Para.sub());
-      p.containers.pop();
-    },
-  },
-  Spec {
-    name: "code_block",
-    is_para: false,
-    content: "text",
-    cont: |p| {
-      let m = p.find("^(```)[ \t]*[\r\n]");
-      if m.is_match {
-        p.pos = m.end - 1;
-        p.finished_line = true;
-        false
-      } else {
-        true
-      }
-    },
-    open: |p| {
-      if !p.subject[p.pos..].starts_with("```\n") {
-        return false;
-      }
-      p.add_container(Container {
-        spec: &SPECS[1],
-        inline_parser: inline::Parser::new(p.subject.clone(), p.opts.clone(), p.warn.clone()),
-        indent: 0,
-      });
-      p.add_match(p.pos, p.pos + 3, Comp::CodeBlock.add());
-      p.pos = p.pos + 2;
+struct CodeBlock {}
+
+impl Container for CodeBlock {
+  fn content(&self) -> &'static str {
+    "text"
+  }
+  fn open(p: &mut Parser) -> Option<Box<dyn Container>>
+  where
+    Self: Sized,
+  {
+    if !p.subject[p.pos..].starts_with("```\n") {
+      return None;
+    }
+    p.add_match(p.pos, p.pos + 3, Comp::CodeBlock.add());
+    p.pos = p.pos + 2;
+    p.finished_line = true;
+    Some(Box::new(CodeBlock {}))
+  }
+
+  fn cont(&mut self, p: &mut Parser) -> bool {
+    let m = p.find("^(```)[ \t]*[\r\n]");
+    if m.is_match {
+      p.pos = m.end - 1;
       p.finished_line = true;
+      false
+    } else {
       true
-    },
-    close: |p| {
-      p.add_match(p.pos - 3, p.pos, Comp::CodeBlock.sub());
-      p.containers.pop();
-    },
-  },
-];
+    }
+  }
+
+  fn close(self: Box<Self>, p: &mut Parser) {
+    p.add_match(p.pos - 3, p.pos, Comp::CodeBlock.sub());
+  }
+}
 
 impl Parser {
   pub fn new(mut subject: String, opts: ParseOpts, warn: Option<Warn>) -> Parser {
@@ -121,11 +116,6 @@ impl Parser {
     res
   }
 
-  fn get_inline_matches(&mut self) {
-    let matches = self.containers.last_mut().unwrap().inline_parser.get_matches();
-    self.matches.extend(matches);
-  }
-
   fn find(&self, pat: &'static str) -> PatMatch<'static> {
     find_at(&self.subject, pat, self.pos)
   }
@@ -134,15 +124,15 @@ impl Parser {
     self.matches.push(Match::new(startpos..endpos, annot))
   }
 
-  fn add_container(&mut self, container: Container) {
-    let last_matched = self.last_matched_container;
-    while self.containers.len() > last_matched
-      || (self.containers.len() > 0 && self.containers.last().unwrap().spec.content != "block")
-    {
-      self.containers.last().unwrap().spec.close(self)
-    }
-    self.containers.push(container)
-  }
+  //  fn add_container(&mut self, container: Container) {
+  //    let last_matched = self.last_matched_container;
+  //    while containers.len() > last_matched
+  //      || (containers.len() > 0 && containers.last().unwrap().spec.content != "block")
+  //    {
+  //      containers.last().unwrap().spec.close(self)
+  //    }
+  //    containers.push(container)
+  //  }
 
   fn skip_space(&mut self) {
     let m = find_at(&self.subject, "[^ \t]", self.pos);
@@ -162,8 +152,8 @@ impl Parser {
   }
 
   pub fn parse(&mut self) {
-    let specs = SPECS;
-    let para_spec = &specs[0];
+    let mut containers: Vec<Box<dyn Container>> = Vec::new();
+
     let subjectlen = self.subject.len();
     while self.pos < subjectlen {
       self.indent = 0;
@@ -173,11 +163,10 @@ impl Parser {
 
       // check open containers for continuation
       self.last_matched_container = 0;
-      for idx in 0..self.containers.len() {
+      for idx in 0..containers.len() {
         // skip any indentation
         self.skip_space();
-        let container = self.containers[idx].spec;
-        if container.cont(self) {
+        if containers[idx].cont(self) {
           self.last_matched_container = idx + 1
         } else {
           break;
@@ -186,8 +175,8 @@ impl Parser {
 
       // if we hit a close fence, we can move to next line
       if self.finished_line {
-        while self.containers.len() > self.last_matched_container {
-          self.containers.last().unwrap().spec.close(self)
+        while containers.len() > self.last_matched_container {
+          containers.pop().unwrap().close(self)
         }
       }
 
@@ -197,27 +186,27 @@ impl Parser {
         let mut is_blank = self.pos == self.starteol;
 
         let mut new_starts = false;
-        let last_match = self.containers[..self.last_matched_container].first();
+        let last_match = containers[..self.last_matched_container].first();
         let mut check_starts = !is_blank
-          && !matches!(last_match, Some(c) if c.spec.content != "block")
+          && !matches!(last_match, Some(c) if c.content() != "block")
           && !self.find("^%a+%s").is_match; // optimization
 
         while check_starts {
           check_starts = false;
-          for i in 0..specs.len() {
-            let spec = &specs[i];
-            if !spec.is_para {
-              if spec.open(self) {
-                self.last_matched_container = self.containers.len();
-                if self.finished_line {
-                  check_starts = false
-                } else {
-                  self.skip_space();
-                  new_starts = true;
-                  check_starts = spec.content != "text"
-                }
-                break;
+          for i in 1..CONTAINERS.len() {
+            let open = CONTAINERS[i];
+            if let Some(cont) = open(self) {
+              let content = cont.content();
+              containers.push(cont);
+              self.last_matched_container = containers.len();
+              if self.finished_line {
+                check_starts = false
+              } else {
+                self.skip_space();
+                new_starts = true;
+                check_starts = content != "text"
               }
+              break;
             }
           }
         }
@@ -230,38 +219,39 @@ impl Parser {
 
           let is_lazy = !is_blank
             && !new_starts
-            && self.last_matched_container < self.containers.len()
-            && self.containers.last().unwrap().spec.content == "inline";
+            && self.last_matched_container < containers.len()
+            && containers.last().unwrap().content() == "inline";
 
-          if !is_lazy && self.last_matched_container < self.containers.len() {
-            while self.containers.len() > self.last_matched_container {
-              self.containers.last().unwrap().spec.close(self);
+          if !is_lazy && self.last_matched_container < containers.len() {
+            while containers.len() > self.last_matched_container {
+              containers.pop().unwrap().close(self);
             }
           }
 
           // add para by default if there's text
-          if !matches!(self.containers.last(), Some(c) if c.spec.content != "block") {
+          if !matches!(containers.last(), Some(c) if c.content() != "block") {
             if is_blank {
               if !new_starts {
                 // need to track these for tight/loose lists
                 self.add_match(self.pos, self.endeol, Atom::Blankline);
               }
             } else {
-              para_spec.open(self);
+              let para = CONTAINERS[0](self).unwrap();
+              containers.push(para);
             }
           }
 
-          if let Some(tip) = self.containers.last_mut() {
-            if tip.spec.content == "text" {
-              let mut startpos = self.pos;
-              if self.indent > tip.indent {
-                // get back the leading spaces we gobbled
-                startpos = startpos - (self.indent - tip.indent)
-              }
+          if let Some(tip) = containers.last_mut() {
+            if tip.content() == "text" {
+              let startpos = self.pos;
+              // if self.indent > tip.indent {
+              //   // get back the leading spaces we gobbled
+              //   startpos = startpos - (self.indent - tip.indent)
+              // }
               self.add_match(startpos, self.endeol, Atom::Str)
-            } else if tip.spec.content == "inline" {
+            } else if let Some(inline_parser) = tip.inline_parser() {
               if !is_blank {
-                tip.inline_parser.feed(self.pos, self.endeol)
+                inline_parser.feed(self.pos, self.endeol)
               }
             }
           }
@@ -270,13 +260,13 @@ impl Parser {
 
       self.pos = self.endeol;
     }
-    self.finish()
+    self.finish(containers)
   }
 
-  fn finish(&mut self) {
+  fn finish(&mut self, mut containers: Vec<Box<dyn Container>>) {
     // close unmatched containers
-    while let Some(cont) = self.containers.last() {
-      cont.spec.close(self)
+    while let Some(cont) = containers.pop() {
+      cont.close(self)
     }
     if self.opts.debug_matches {
       for &m in &self.matches {
